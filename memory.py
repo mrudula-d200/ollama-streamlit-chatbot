ab7a16129e1e1b9a78d29ddff5c13d6bc2ad0bb0
mrudula-d200 <mrudula232007@gmail.com>
2026-09-24 12:17:36 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,1 +1,22 @@
-
+import ollama
+list=[]
+while True:
+    user_input=input("Ask Something : ")
+    if user_input == "exit":
+        print("Goodbye")
+        break
+    else:
+        list.append(
+            {"role" : "user","content" : user_input}
+                  )
+        response=ollama.chat(
+            model="llama3.2:3b",
+            messages=list
+        )
+        bot_response=response["message"]["content"]
+        list.append({"role" : "assistant","content" : bot_response})
+        print(bot_response)
+    for message in messages: 
+        role=message['role']
+        content=message['content']
+        print(role,content)
